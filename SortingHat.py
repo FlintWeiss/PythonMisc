88e3acf517b48b7d2fe5618fe4ad7a37bf64d75a
Flint Weiss <cottonwood@squidgey.com>
2018-02-24 16:02:26 -0800

    Working PoC of Sorting Hat (4 hours)
    Created to load onto my ChimeIn Pi prototype device, a Raspbery Pi with
    a button and LED array attached

---
+++ SortingHat.py
@@ -0,0 +1,18 @@
+import collections, random
+
+houseList = ["1", "2", "3", "4", "1", "2", "3", "4", "1", "2", "3", "4"]
+pickList = []
+
+# will replace this with "button press action"
+for i in range(15):
+   if pickList:
+      # pick a house by removing one from list
+      print ("Pick:", pickList.pop() )
+      
+   else:
+      # rebuild pickList by pre-randomizing the list
+      pickList = random.sample(houseList, k=12)
+      
+   print (pickList)
+
+
